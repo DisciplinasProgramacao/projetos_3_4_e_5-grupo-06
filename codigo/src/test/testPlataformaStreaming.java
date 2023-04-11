abe08fd267e04eafe7ea137ed18470dc88de89c0
1385832 <1385832@PPL1INFLB050702.PUCMINAS.NET>
2023-04-11 09:29:35 -0300

    Criacao do projeto

---
+++ codigo/src/test/testPlataformaStreaming.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testPlataformaStreaming {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ codigo/src/test/testSerie.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testSerie {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
