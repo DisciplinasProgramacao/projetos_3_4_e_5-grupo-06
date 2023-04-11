38c81d6e7b4ab983a8b0c14b590ccbc7868163c1
gscantoni <104282001+gscantoni@users.noreply.github.com>
2023-04-11 10:36:58 -0300

    Criação de caso de teste
    Adicionar e remover

--- codigo/src/test/testCliente.java
+++ codigo/src/test/testCliente.java
@@ -6,9 +6,42 @@ import org.junit.jupiter.api.Test;
 
 class testCliente {
 
+	private Cliente cliente;
+    	private Serie serie1;
+    	private Serie serie2;
+    	private Serie serie3;
+    
+    @BeforeEach
+    public void setup() {
+        cliente = new Cliente("cliente123", "senha123");
+        serie1 = new Serie("Game of Thrones", "Fantasia", "Inglês", 8);
+        serie2 = new Serie("Breaking Bad", "Drama", "Inglês", 5);
+        serie3 = new Serie("La Casa de Papel", "Ação", "Espanhol", 4);
+    }
+    
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void retirarDaListaNaoRemoveSeNaoExistir() {
+        cliente.adicionarNaLista(serie1);
+        cliente.adicionarNaLista(serie2);
+        cliente.adicionarNaLista(serie3);
+        
+        cliente.retirarDaLista("Friends");
+        
+        Assertions.assertEquals(3, cliente.getListaParaVer().size());
+    }
+	
+	@Test
+	public void adicionarNaListaAdicionaSerieNaLista() {
+        cliente.adicionarNaLista(serie1);
+        cliente.adicionarNaLista(serie2);
+        cliente.adicionarNaLista(serie3);
+        
+        Assertions.assertTrue(cliente.getListaParaVer().contains(serie1));
+        Assertions.assertTrue(cliente.getListaParaVer().contains(serie2));
+        Assertions.assertTrue(cliente.getListaParaVer().contains(serie3));
+        
+		
 	}
+
 
 }
