e190394c386c6afaa429a3f9c8cf1929a8a73b4c
gscantoni <104282001+gscantoni@users.noreply.github.com>
2023-04-11 09:58:20 -0300

    Criação da classe cliente #1

--- codigo/src/business/Cliente.java
+++ codigo/src/business/Cliente.java
@@ -1,5 +1,45 @@
 package business;
 
-public class Cliente {
+import java.util.ArrayList;
+import java.util.List;
 
-}
+public class Cliente {
+    private String nomeDeUsuario;
+    private String senha;
+    private List<Serie> listaParaVer;
+    private List<Serie> listaJaVistas;
+    
+    public Cliente(String nomeDeUsuario, String senha) {
+        this.nomeDeUsuario = nomeDeUsuario;
+        this.senha = senha;
+        this.listaParaVer = new ArrayList<>();
+        this.listaJaVistas = new ArrayList<>();
+    }
+    
+    
+    // get / set
+
+    public String getNomeDeUsuario() {
+		  return nomeDeUsuario;
+  	}
+	  public void setNomeDeUsuario(String nomeDeUsuario) {
+		  this.nomeDeUsuario = nomeDeUsuario;
+	  }
+  	public String getSenha() {
+		  return senha;
+	  }
+	  public void setSenha(String senha) {
+		  this.senha = senha;
+	  }
+  	public List<Serie> getListaParaVer() {
+	  	return listaParaVer;
+	  }
+	  public void setListaParaVer(List<Serie> listaParaVer) {
+	  	this.listaParaVer = listaParaVer;
+	  }
+	  public List<Serie> getListaJaVistas() {
+		  return listaJaVistas;
+	  }
+	  public void setListaJaVistas(List<Serie> listaJaVistas) {
+		  this.listaJaVistas = listaJaVistas;
+	  }
