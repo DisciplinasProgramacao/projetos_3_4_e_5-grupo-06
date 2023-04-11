df7dc74cdaaccb843b8d93ec2c0f7b27cddc8919
Victor Friche <104036970+friche11@users.noreply.github.com>
2023-04-11 10:28:06 -0300

    Teste metodo registrarAudiencia de serie #4

--- codigo/src/test/testSerie.java
+++ codigo/src/test/testSerie.java
@@ -2,13 +2,24 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class testSerie {
+import business.Serie;
 
+class testSerie {
+	
+	Serie s;
+	
+	@BeforeEach
+	public void setUp() {
+		s = new Serie();
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testRegistrarAudiencia(){
+		s.registrarAudiencia();
+		assertEquals(s.getAudiencia(), 1);
 	}
 
 }
