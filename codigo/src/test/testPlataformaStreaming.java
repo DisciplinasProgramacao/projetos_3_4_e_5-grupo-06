0dcbf7435a3d65ad1202941a11ad196602fe21c7
luanaf4 <luana.fleury4@gmail.com>
2023-04-14 17:44:30 -0300

    Criação classe e testes PlataformaStreaming #8

--- codigo/src/test/testPlataformaStreaming.java
+++ codigo/src/test/testPlataformaStreaming.java
@@ -1,14 +1,29 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import business.PlataformaStreaming;
+import business.Cliente;
+import business.Serie;
 
 class testPlataformaStreaming {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	private PlataformaStreaming plataforma;
 
+    @BeforeEach
+    public void setUp() {
+        plataforma = new PlataformaStreaming("Netflix");
+    }
+
+	// teste login
+    @Test
+    public void testLogin() {
+        Cliente cliente = new Cliente("user123", "password");
+        plataforma.adicionarCliente(cliente);
+
+        Assertions.assertNull(plataforma.login("user123", "wrongpassword"));
+        Assertions.assertEquals(cliente, plataforma.login("user123", "password"));
+        Assertions.assertEquals(cliente, plataforma.getClienteAtual());
+    }
 }
