ce4abb9bafcd85daabfe0aa821bedfc6efd05065
luanaf4 <luana.fleury4@gmail.com>
2023-04-14 16:28:16 -0300

    Atualização testes da classe Cliente

--- codigo/src/test/testCliente.java
+++ codigo/src/test/testCliente.java
@@ -1,10 +1,13 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import business.Cliente;
 import business.Serie;
@@ -16,12 +19,12 @@ class testCliente {
     	private Serie serie2;
     	private Serie serie3;
     
-    @BeforeEach
+    @Before
     public void setup() {
         cliente = new Cliente("cliente123", "senha123");
-        serie1 = new Serie("Game of Thrones", "Fantasia", "Inglês", 8);
-        serie2 = new Serie("Breaking Bad", "Drama", "Inglês", 5);
-        serie3 = new Serie("La Casa de Papel", "Ação", "Espanhol", 4);
+        serie1 = new Serie("Game of Thrones", "Fantasia", "Inglês", 62, 0);
+        serie2 = new Serie("Breaking Bad", "Drama", "Inglês", 73, 0);
+        serie3 = new Serie("La Casa de Papel", "Ação", "Espanhol", 24, 0);
     }
     
 	@Test
@@ -46,4 +49,27 @@ class testCliente {
         Assertions.assertTrue(cliente.getListaParaVer().contains(serie3));
 	}
 
+    @Test
+    public void filtrarPorGenerotest(){
+        cliente.registrarAudiencia(serie2);
+        List<Serie> resultado = cliente.filtrarporGenero("Drama");
+        assertEquals(1, resultado.size());
+        assertTrue(resultado.contains(serie2));
+    }
+
+    @Test
+    public void fitrarPorIdioma(){
+        cliente.registrarAudiencia(serie1);
+        List<Serie> resultado = cliente.filtrarPorIdioma("Inglês");
+        assertEquals(1, resultado.size());
+        assertTrue(resultado.contains(serie1));
+    }
+
+    @Test
+	public void testFiltrarPorQtdEpisodios() {
+		cliente.registrarAudiencia(serie1);
+		List<Serie> resultado = cliente.filtrarPorQtdEpisodios(62);
+		assertEquals(1, resultado.size());
+		assertTrue(resultado.contains(serie1));
+	}
 }
