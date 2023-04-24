4faed93f69d696a9bc2e29eba8a9b01fd03d031c
luanaf4 <luana.fleury4@gmail.com>
2023-04-24 14:57:38 -0300

    Atualização classe Filme #12

--- codigo/src/business/Filme.java
+++ codigo/src/business/Filme.java
@@ -1,5 +1,25 @@
 package business;
 
 public class Filme {
+    private int idFilme;
+    private String nome;
+    private String dataLancamento;
+    private int duracao;
     
+    // construtor
+    public Filme(int idFilme, String nome, String dataLancamento, int duracao) {
+        this.idFilme = idFilme;
+        this.nome = nome;
+        this.dataLancamento = dataLancamento;
+        this.duracao = duracao;
+    }
+
+    //getters and setters
+    public int getId() {
+        return idFilme;
+    }
+    
+    public void setId(int id) {
+        this.idFilme = id;
+    }
 }
--- codigo/src/test/testFilme.java
+++ codigo/src/test/testFilme.java
@@ -1,5 +1,22 @@
 package test;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+import business.*;
+
 public class testFilme {
+
+    //teste para o getIdFilme
+    @Test
+    public void testGetIdFilme() {
+        Filme filme = new Filme(1, "Filme 1", "01/01/2023", 120);
+        assertEquals(1, filme.getId());
+    }
     
+    @Test
+    public void testSetIdFilme() {
+        Filme filme = new Filme(1, "Filme 1", "01/01/2023", 120);
+        filme.setId(2);
+        assertEquals(2, filme.getId());
+    }
 }
