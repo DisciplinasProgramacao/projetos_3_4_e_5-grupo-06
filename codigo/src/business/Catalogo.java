162ea83bc1093b9f904ab2db703b7c8e0cf5400f
luanaf4 <luana.fleury4@gmail.com>
2023-04-25 10:12:52 -0300

    Atualização carregarFilmes #12

--- codigo/src/business/Catalogo.java
+++ codigo/src/business/Catalogo.java
@@ -8,45 +8,42 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Catalogo {
-        private List<Filme> filmes;
+    private List<Filme> filmes;
 
-        public Catalogo() {
-            this.filmes = new ArrayList<>();
-        }
-        
-        public void carregarFilmes() {
-            String caminhoArquivo = "C:\\Users\\dtiDIgital\\Downloads\\projetos_3_4_e_5-grupo-06-master (2)\\projetos_3_4_e_5-grupo-06\\codigo\\POO_Filmes.csv";
+    public Catalogo() {
+        this.filmes = new ArrayList<>();
+    }
+
+    public void carregarFilmes() {
+            String caminhoArquivo = "C:\\Users\\dtiDIgital\\Downloads\\projetos_3_4_e_5-grupo-06-master (2)\\projetos_3_4_e_5-grupo-06\\codigo\\filmes.csv";
+            Scanner sc;
             try {
-                Scanner sc = new Scanner(new File(caminhoArquivo));
-                sc.useDelimiter(";");
-                while (sc.hasNext()) {
-                    try {
-                        int idFilme = sc.nextInt();
-                        String nome = sc.next();
-                        String dataLancamento = sc.next();
-                        int duracao = sc.nextInt();
-                        Filme filme = new Filme(idFilme, nome, dataLancamento, duracao);
-                        filmes.add(filme);
-                    } catch (InputMismatchException e) {
-                        System.out.println("Erro ao ler o arquivo: " + e.getMessage());
-                        System.out.println("Token inválido: " + sc.next());
-                    }
-                }
+                sc = new Scanner(new File(caminhoArquivo));
+                while(
+                    sc.hasNext()
+                ){
+                    String a = sc.nextLine();
+                    String elementos[]= a.split(";");
+                    for(int i= 0; i< elementos.length; i++)
+
+
+                }      
                 sc.close();
                 System.out.println("Arquivo carregado com sucesso.");
-            } catch (IOException e) {
+            } catch (InputMismatchException e) {
+                System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+            }catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        
-        //getters and setters
 
-        public List<Filme> getFilmes() {
-            return filmes;
-        }
+    // getters and setters
 
-        public void setFilmes(List<Filme> filmes) {
-            this.filmes = filmes;
-        }
-    }   
+    public List<Filme> getFilmes() {
+        return filmes;
+    }
 
+    public void setFilmes(List<Filme> filmes) {
+        this.filmes = filmes;
+    }
+}
