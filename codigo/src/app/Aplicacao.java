eadf3586f76ac485a420d48adb5acbb2c76b08fa
Guilherme Cantoni <104282001+gscantoni@users.noreply.github.com>
2023-05-02 20:44:42 -0300

    Correção do menu

--- codigo/src/app/Aplicacao.java
+++ codigo/src/app/Aplicacao.java
@@ -3,7 +3,6 @@ package app;
 
 import java.io.IOException;
 import java.util.Scanner;
-import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import business.*;
@@ -15,18 +14,17 @@ public class Aplicacao {
     
     	PlataformaStreaming plataforma = new PlataformaStreaming("testar"); 
         
-		Scanner scanner = new Scanner(System.in);
+    	DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+        Scanner scanner = new Scanner(System.in);
         
-		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     	
-		while (true) {
+	while (true) {
             System.out.println("Menu:");
             System.out.println("1. Adicionar série");
             System.out.println("2. Adicionar filme"); 
             System.out.println("3. Adicionar cliente");
             System.out.println("4. Fazer login");
-            System.out.println("5. Carregar lista de filmes");
-            System.out.println("6. Carregar lista de séries");
             System.out.println("0. Sair");
 
             System.out.print("Escolha uma opção: ");
@@ -37,25 +35,23 @@ public class Aplicacao {
             case 1:
             
             System.out.println("Digite o Nome da série:");
-            int nome = scanner.nextInt();
-            scanner.nextLine(); 
-            
+            String nome = scanner.nextLine();
+           
             System.out.println("Digite o Genero da série:");
-            int genero = scanner.nextInt();
-            scanner.nextLine(); 
+            String genero =  scanner.nextLine();
             
             System.out.println("Digite o Idioma da série:");
-            int idioma = scanner.nextInt();
-            scanner.nextLine(); 
-            
+            String idioma =  scanner.nextLine();
+                       
             System.out.println("Digite a Quantidade de episodios da série:");
             int quantidadeEpisodios = scanner.nextInt();
             scanner.nextLine(); 
             
-            System.out.println("Digite a data de lançamento no formato dd/MM/yyyy:");
-            LocalDate dataLancamento = LocalDate.parse(scanner.nextLine(), dateFormatter);
+            System.out.println("Digite a audiencia da série:");
+            int audiencia = scanner.nextInt();
+            scanner.nextLine(); 
             
-            Serie serie = new Serie(nome, genero, idioma, quantidadeEpisodios, dataLancamento);
+            Serie serie = new Serie(nome, genero, idioma, quantidadeEpisodios, audiencia);
             plataforma.adicionarSerie(serie);
             System.out.println("Série adicionada com sucesso!");
             
@@ -64,22 +60,22 @@ public class Aplicacao {
             case 2:
             	
             	System.out.println("Digite o Id do filme:");
-                int idDoFilme = scanner.nextInt();
+                int idFilme = scanner.nextInt();
                 scanner.nextLine(); 
                 
             	System.out.println("Digite o Nome do filme:");
-            	String nomeDoFilme = scanner.nextInt();
-                scanner.nextLine();      
+            	String nomeDoFilme =  scanner.nextLine();
+                     
+                System.out.println("Digite a data de lançamento no formato dd/MM/yyyy:");
+                String dataLancamento =  scanner.nextLine();
                 
                 System.out.println("Digite a duracao do filme:");
-                String duracao = scanner.nextInt();
-                scanner.nextLine();       
-                
-                System.out.println("Digite a data de lançamento no formato dd/MM/yyyy:");
-                LocalDate dataLancamentoDoFilme = LocalDate.parse(scanner.nextLine(), dateFormatter);
+                int duracao =  scanner.nextInt();
+                scanner.nextLine();      
+                            
             	
-                Filme filme = new Filme(idDoFilme, nomeDoFilme, duracao, dataLancamentoDoFilme);
-                plataforma.adicionarFilme(Filme);
+                Filme filme = new Filme(idFilme, nomeDoFilme, dataLancamento, duracao);
+                plataforma.adicionarFilmes(filme);
                 System.out.println("Filme adicionado com sucesso!");
             	
             	break;
@@ -100,11 +96,13 @@ public class Aplicacao {
             	
             case 4:
             	
-            	//	System.out.println("Insira o nome de usuario:");
-            	//   String nomeUsuario = scanner.nextLine();
+            	System.out.println("Insira o nome de usuario:");
+                String nomeUsuario = scanner.nextLine();
                 
-            	//  System.out.println("Insira a senha:");
-            	// String senha = scanner.nextLine();
+                System.out.println("Insira a senha:");
+                String senha = scanner.nextLine();
+                
+                plataforma.login(nomeUsuario, senha);
             	
                 //if (nomeUsuario) == null){
                 //	System.out.println("Erro no login: usario nao encontrado");
@@ -121,17 +119,17 @@ public class Aplicacao {
             	Catalogo catalogo = new Catalogo();
             	catalogo.carregarFilmes();
             	System.out.println("Lista de filmes:");
-            	for (Filme filme : catalogo.getFilmes()) {
-            		System.out.println(filme.getId() + ", " + filme.getNome() + ", " + filme.getDataLancamento() + ", "+ filme.getDuracao());                 
+            	for (Filme filme1 : catalogo.getFilmes()) {
+            		System.out.println(filme1.getId() + ", " + filme1.getNome() + ", " + filme1.getDataLancamento() + ", "+ filme1.getDuracao());                 
         }  	
             	break;
             	
             case 6:
-            	// Carregar lista de séries
+            	// Carregar lista de séries            	
             	catalogo.carregarSeries();
             	System.out.println("Lista de séries:");
-            	for (Serie serie : catalogo.getSerie()) {
-        		System.out.println(serie.getId() + ", " + serie.getNome() + ", " + serie.getDataLancamento());
+            	for (Serie serie1 : catalogo.getSerie()) {
+        		System.out.println(serie1.getId() + ", " + serie1.getNome() + ", " + serie1.getDataLancamento());
             	}
             	break;
             case 0:
@@ -154,3 +152,5 @@ public class Aplicacao {
     }
 
 }
+}
+
