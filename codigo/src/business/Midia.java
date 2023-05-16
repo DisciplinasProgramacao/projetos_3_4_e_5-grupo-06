95b70320c224bdaa76602d47d5c31ca2880a69cf
luanaf4 <luana.fleury4@gmail.com>
2023-05-16 08:28:01 -0300

    Atualização isEspecialista#24

--- codigo/src/business/Midia.java
+++ codigo/src/business/Midia.java
@@ -12,9 +12,12 @@ public class Midia {
      * @return true se o cliente é especialista, false se o cliente é regular.
      */
     public boolean isEspecialista(Cliente cliente) {
-        int quantidadeMinima = 5; // Defina a quantidade mínima de mídias para um cliente ser considerado especialista
+        int quantidadeMinima = 5; //quantidade mínima de mídias para um cliente ser considerado especialista
         
         List<Serie> listaJaVistas = cliente.getListaJaVistas();
         return listaJaVistas.size() >= quantidadeMinima;
+
+        //falta definir tempo minimo de um mes
+
     }
 }
