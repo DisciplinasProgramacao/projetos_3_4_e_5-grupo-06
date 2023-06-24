e83700146af98ea115ece29f9407e96feb75b3e4
Guilherme Cantoni <104282001+gscantoni@users.noreply.github.com>
2023-06-24 13:49:27 -0300

    Update Aplicacao.java

--- codigo/src/app/Aplicacao.java
+++ codigo/src/app/Aplicacao.java
@@ -1,237 +1,283 @@
 package app;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import business.*;
-import business.composition.Cliente;
+import business.exceptions.ClienteUnauthorizedPermission;
+import business.exceptions.MidiaNotFoundException;
 
 public class Aplicacao {
+	
+	/**
+	 * ATENÇÃO
+	 * 
+	 * Pode ser que, ao carregar o arquivo, dependendo de qual IDE você esteja utilizando
+	 * O caminho do arquivo não será lido corretamente
+	 * O programa foi desenvolvido no Eclipse, no entanto, ao tentar rodar no
+	 * Visual Studio Code, foi necessario adicionar contrabarras nas strings
+	 * de caminho de arquivo ao invés de barras comuns
+	 * 
+	 */
 
 	public static void main(String[] args)throws IOException {
 
-
-		PlataformaStreaming plataforma = new PlataformaStreaming("Goflix");         
-		Catalogo catalogo = new Catalogo();
-
+		PlataformaStreaming plataforma = new PlataformaStreaming("Goflix");        
+		
+//		plataforma.login("Nin154859", "NYor07361");
+		
+		while (plataforma.getClienteLogado() == null) {
+			plataforma.logar();
+		}
+		
+		try {
+			Thread.sleep(1500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 		Scanner scanner = new Scanner(System.in);
-	
-
+		
+		boolean close = false;
 
 		while (true) {
 			System.out.println("Menu:");
-			System.out.println("1. Adicionar série");
-			System.out.println("2. Adicionar filme"); 
+			System.out.println();
+			System.out.println("----- Gerenciar");
+			System.out.println();
+			System.out.println("1. Registrar série");
+			System.out.println("2. Registrar filme"); 
 			System.out.println("3. Registrar cliente");
-			System.out.println("4. Fazer login");
-			System.out.println("5. Carregar lista de filmes");
-			System.out.println("6. Carregar lista de séries");
-			System.out.println("7. Carregar audiência");
-			System.out.println("8. Carregar espectadores");
-			System.out.println("9. Filtrar por genero");
-			System.out.println("10. Filtrar por idioma");
-			System.out.println("11. Filtrar por quantidade de episodios");
-			System.out.println("12. Qual cliente assistiu mais mídias");
+			System.out.println();
+			System.out.println("--- Cliente");
+			System.out.println();
+			System.out.println("4. Assistir midia");
+			System.out.println("5. Avaliar midia");
+			System.out.println("6. Adicionar midia na lista para assistir");
+			System.out.println("7. Adicionar midia na lista de assistidos");
+			System.out.println("8. Ver lista de midias para assistir");
+			System.out.println("9. Ver lista de midias assistidas");
+			System.out.println("10. Filtrar minha lista de midias para assistir");
+			System.out.println();
+			System.out.println("--- Plataforma");
+			System.out.println();
+			System.out.println("11. Fazer login com outro cliente");
+			System.out.println("12. Fazer cliente logado virar profissional");
+			System.out.println("13. Ver avaliações do cliente logado");
+			System.out.println("14. Ver lista de todas as midias disponiveis do catalogo");
+			System.out.println("15. Ver audiencia de uma midia");
+			System.out.println("16. Encontrar midia pelo nome");
+			System.out.println();
+			System.out.println("----- Filtros");
+			System.out.println();
+			System.out.println("17. Qual cliente assistiu mais midias e quantas midias");
+			System.out.println("18. Qual cliente tem mais avaliações e quantas avaliações");
+			System.out.println("19. Qual a porcentagem de clientes com pelo menos 15 avaliações ");
+			System.out.println("20. Quais são as 10 midias mais vistas");
+			System.out.println("21. Quais são as 10 midias mais vistas por genero");
+			System.out.println("22. Quais são as 10 midias com melhor avaliação (minimo 100 avaliações)");
+			System.out.println("23. Quais são as 10 midias com melhor avaliação (minimo 100 avaliações) por genero");
+			System.out.println();
 			System.out.println("0. Sair");
+			System.out.println();
 
 			System.out.print("Escolha uma opção: ");
-			int opcao = scanner.nextInt();
-			scanner.nextLine();
-
-			switch (opcao) {
+			
+			String opcao = null;
+			int op = -1;
+			
+			try {
+			opcao = scanner.nextLine();
+			op = Integer.valueOf(opcao);
+			} catch (InputMismatchException | NumberFormatException e) {
+				System.out.println("Opcao invalida");
+			}
+			
+			switch (op) {
 			case 1:
-
-				System.out.println("Digite o Nome da série:");
-				String nome = scanner.nextLine();
-
-				System.out.println("Digite o Genero da série:");
-				String genero =  scanner.nextLine();
-
-				System.out.println("Digite o Idioma da série:");
-				String idioma =  scanner.nextLine();
-
-				System.out.println("Digite a Quantidade de episodios da série:");
-				int quantidadeEpisodios = scanner.nextInt();
-				scanner.nextLine(); 
-
-				System.out.println("Digite a audiencia da série:");
-				int audiencia = scanner.nextInt();
-				scanner.nextLine(); 
-
-				Serie serie = new Serie(nome, genero, idioma, quantidadeEpisodios, audiencia);
-				plataforma.adicionarSerie(serie);
-				System.out.println("Série adicionada com sucesso!");
+				
+				plataforma.adicionarSerie();
 
 				break;
 
 			case 2:
-
-				System.out.println("Digite o Id do filme:");
-				int idFilme = scanner.nextInt();
-				scanner.nextLine(); 
-
-				System.out.println("Digite o Nome do filme:");
-				String nomeDoFilme =  scanner.nextLine();
-
-				System.out.println("Digite a data de lançamento no formato dd/MM/yyyy:");
-				String dataLancamento =  scanner.nextLine();
-
-				System.out.println("Digite a duracao do filme:");
-				int duracao =  scanner.nextInt();
-				scanner.nextLine();      
-
-
-				Filme filme = new Filme(idFilme, nomeDoFilme, dataLancamento, duracao);
-				plataforma.adicionarFilmes(filme);
-				System.out.println("Filme adicionado com sucesso!");
+				
+				plataforma.adicionarFilme();
 
 				break;
 
 			case 3:
 
-				System.out.println("Digite o nome de usuário do cliente:");
-				String nomeDeUsuario = scanner.nextLine();
-
-				System.out.println("Digite a senha do cliente:");
-				String senhaDeUsuario = scanner.nextLine();
-
-				Cliente cliente = new Cliente(nomeDeUsuario, senhaDeUsuario);
-				plataforma.adicionarCliente(cliente);
-				System.out.println("Cliente adicionado com sucesso!");
+				plataforma.adicionarCliente();
 
 				break;
 
 			case 4:
 
-				System.out.println("Insira o nome de usuario:");
-				String nomeUsuario = scanner.nextLine();
-
-				System.out.println("Insira a senha:");
-				String senha = scanner.nextLine();
-
-				Cliente clienteLogado = plataforma.login(nomeUsuario, senha);
-
-				if (nomeUsuario == null){
-					System.out.println("Erro no login: usario nao encontrado");
-					System.out.println();
+				try {
+					plataforma.assistirMidia();
+				} catch (MidiaNotFoundException | ClienteUnauthorizedPermission e) {
+					System.out.println(e.getMessage());
 				}
-				if (senha == null) {
-					System.out.println("Erro no login: senha incorreta");
-				}
-				else {
-					System.out.println("Login realizado com sucesso");
-			        // Registra a visualização de mídias pelo cliente logado
-			        plataforma.registrarVisualizacao(clienteLogado);
-				}
-				System.out.println();
+				
 				break;
 			case 5:
-				// Carregando lista de filmes
-
-				catalogo.carregarFilmes();
-				System.out.println("Lista de filmes:");
-				for (Filme filme1 : catalogo.getFilmes()) {
-					System.out.println(filme1.getId() + ", " + filme1.getNome() + ", " + filme1.getDataLancamento() + ", "+ filme1.getDuracao());                 
-				}  	
+				
+				try {
+					plataforma.avaliarMidia();
+				} catch (MidiaNotFoundException e) {
+					System.out.println(e.getMessage());
+				}
+				
 				break;
 
 			case 6:
-				// Carregar lista de séries 
-
-
-				catalogo.carregarSeries();
-				System.out.println("Lista de séries:");
-				for (Serie serie1 : catalogo.getSerie()) {
-					System.out.println(serie1.getId() + ", " + serie1.getNome() + ", " + serie1.getDataLancamento());
+				
+				try {
+					plataforma.adicionarMidiaParaAssistir();
+				} catch (MidiaNotFoundException e) {
+					System.out.println(e.getMessage());
 				}
+				
 				break;
 
 			case 7:
-				//Carregando lista de audiência
-
-				System.out.println("Lista de audiência:");
-				plataforma.carregarAudiencia("audiencia.csv");
+				
+				try {
+					plataforma.adicionarNaListaDeAssistidos();
+				} catch (MidiaNotFoundException e) {
+					System.out.println(e.getMessage());
+				}
 
 				break;
 			case 8:
-				//Carregando lista de espectadores
-
-				System.out.println("Lista de espectadores");
-				plataforma.carregarEspectadores("espectador.csv");
+				
+				plataforma.verListaMidiasParaAssistir();
 
 				break;
 			case 9:
 				
-				System.out.println("1. Filtrar serie");
-				System.out.println("2. Filtrar filme");
-				int opcao2 = scanner.nextInt();
-				scanner.nextLine();
-				if(opcao2==1) {
-					System.out.println("Digite o genero");
-					String genero2 =  scanner.nextLine();
-					plataforma.filtrarPorGenero(genero2);
-				}else if(opcao2==2) {
-					System.out.println("Digite o genero");
-				    String genero2 =  scanner.nextLine();
-					plataforma.filtrarPorGenero(genero2);
-				}else {
-					System.out.println("Digite uma opcao valida");
-					break;
-				}
+				plataforma.verListaMidiasAssistidas();
+				
+				break;
+				
 			case 10:
 				
-				System.out.println("1. Filtrar serie");
-				System.out.println("2. Filtrar filme");
-				 opcao2 = scanner.nextInt();
-				scanner.nextLine();
-				if(opcao2==1) {
-					System.out.println("Digite o idioma");
-					idioma =  scanner.nextLine();
-					plataforma.filtrarPorIdioma(idioma);
-				}else if(opcao2==2) {
-					System.out.println("Digite o idioma");
-				     idioma =  scanner.nextLine();
-					plataforma.filtrarPorGenero(idioma);
-				}else {
-					System.out.println("Digite uma opcao valida");
-					break;
-				}
+				plataforma.filtrarMidia();
+				
+				break;
+				
 			case 11:
 				
-				System.out.println("1. Filtrar serie");
-				System.out.println("2. Filtrar filme");
-				 opcao2 = scanner.nextInt();
-				scanner.nextLine();
-				if(opcao2==1) {
-					System.out.println("Digite a quantidade de episodios");
-					int quantidadeEpisodios2 =  scanner.nextInt();
-					plataforma.filtrarPorQtdEpisodios(quantidadeEpisodios2);
-				}else if(opcao2==2) {
-					System.out.println("Digite a quantidade de episodios");
-					int quantidadeEpisodios2 =  scanner.nextInt();
-					plataforma.filtrarPorQtdEpisodios(quantidadeEpisodios2);
-				}else {
-					System.out.println("Digite uma opcao valida");
-					break;
-				}
+				plataforma.logar();
+				
+				break;
 				
 			case 12 :
 				
-				Cliente clienteMaisMidiasAssistidas = plataforma.obterClienteComMaisMidiasAssistidas();
-				if (clienteMaisMidiasAssistidas != null) {
-				    System.out.println("Cliente que assistiu mais mídias: " + clienteMaisMidiasAssistidas.getNomeDeUsuario());
-				    System.out.println("Quantidade de mídias assistidas: " + clienteMaisMidiasAssistidas.getContadorMidiasAssistidas());
-				} else {
-				    System.out.println("Nenhum cliente registrado");
+				plataforma.virarProfissional();
+				
+				break;
+				
+			case 13:
+				
+				plataforma.verAvaliacoesClienteLogado();
+				
+				break;
+				
+			case 14:
+				
+				plataforma.verCatalogoDeMidias();
+				
+				break;
+				
+			case 15:
+				
+				try {
+					plataforma.verAudienciaDeUmaMidia();
+				} catch (MidiaNotFoundException e) {
+					System.out.println(e.getMessage());
 				}
-				System.out.println();
+				
+				break;
+				
+			case 16:
+				
+				try {
+					plataforma.encontrarMidiaPeloNome();
+				} catch (MidiaNotFoundException e) {
+					System.out.println(e.getMessage());
+				}
+				
+				break;
+				
+			case 17:
+				
+				plataforma.qualClienteAssistiuMaisMidias();
+				
+				break;
+				
+			case 18:
+				
+				plataforma.qualClienteTemMaisAvaliacoes();
+				
+				break;
+				
+			case 19:
+				
+				plataforma.qualPorcentagemDeClientesComPeloMenos15Avaliacoes();
+				
+				break;
+				
+			case 20:
+				
+				plataforma.quaisSaoAs10MidiasMaisVistas();
+				
+				break;
+				
+			case 21:
+				
+				plataforma.dezMidiasMaisVistasPorGenero();
+				
+				break;
+				
+			case 22:
+				
+				plataforma.dezMidiasComMelhorAvaliacao();
+				
+				break;
+				
+			case 23:
+				
+				plataforma.dezMidiasComMelhorAvaliacaoPorGenero();
+				
+				break;
+				
 			case 0:
 				System.out.println("Saindo...");
-				scanner.close();
-				return;
+				System.out.println();
+				plataforma.salvar();
+				close = true;
+				break;
+			
+			default:
+				System.out.println("Opcao invalida");
+				break;
 			}
 			
+			if (close)
+				break;
+			
+			System.out.println("");
+			System.out.println("Por favor pressione \"ENTER\" para prosseguir");
+			System.out.println("");
+			
+			scanner.nextLine();
+			
 		}
+		
+		scanner.close();
 	}
 
 }
